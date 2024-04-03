97bac9c87c8e946723eae16b0239f99a0b7c5c86
Iago leal Do Nascimento <iago.l.nascimento@gmail.com>
2024-04-03 20:36:27 -0300

    update

--- formulario.js
+++ formulario.js
@@ -1,12 +1,17 @@
 function adicionarInput() {
     var form = document.getElementById("meuFormulario");
 
+    // Criando um novo rótulo
+    var novoLabel = document.createElement("label");
+    novoLabel.textContent = "Novo Campo:";
+
     // Criando um novo input
     var novoCampo = document.createElement("input");
     novoCampo.type = "text";
     novoCampo.name = "novoCampo"; // Nome do input
     novoCampo.placeholder = "Novo Campo";
 
-    // Adicionando o input ao formulário
+    // Adicionando o rótulo e o input ao formulário
+    form.appendChild(novoLabel);
     form.appendChild(novoCampo);
 }
